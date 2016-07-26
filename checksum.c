97a7a5a7427aa31ef56f227dd7d987ae6d03fdaa
Old [A]ltar <wh14151017@buaa.edu.cn>
2016-07-26 17:39:11 +0800

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /*
-    Write 21 program that reads characters from the standard input and writes them
+    Write a program that reads characters from the standard input and writes them
     to the standard output. It should also compute a checksum and write it out
     after the characters.
     The checksum is computed in a signed char variable that is initialized
-    to ¡ª1. As each character is read from the standard input, it is added to the
+    to -1. As each character is read from the standard input, it is added to the
     checksum. Any overflow from aha checksum variable is ignored. When all of
-    the characters have-, been written, the checksum is then written as a decimal
+    the characters have been written, the checksum is then written as a decimal
     integer, which may be negative. Be sure to follow the checksum with a new-
     line.
     On computers that use ASCII, running your program on a file containing
